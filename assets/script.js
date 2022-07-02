094c0699326a6e2635ada97fc0f6fd0bd7706a31
Sczygelski <avamariescz@gmail.com>
2022-07-01 19:40:48 -0500

    API linking

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,44 @@
-// var getUserRepos = function() {
-//     fetch("https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}")
-// }
 
-// const q= "Madison, WI"
 
-//http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+//6c6a2773e4ad1859b9bab1adec8ab957
 
-var getUserRepos = function() {
-    var getlatlong = "http://api.openweathermap.org/data/2.5/onecall/timemachine?lat=60.99&lon=30.9&dt=1586468027&appid={API key}";
-    fetch(getlatlong).then(function(response) {
-        response.json().then(function(data){
-            console.log(data);
+// {/* <div class="card">
+//   <h3 class="card-header text-uppercase">Search By User</h3>
+//   <form id="user-form" class="card-body">
+//     <label class="form-label" for="username">Username</label>
+//     <input name="username" id="username" type="text" autofocus="true" class="form-input" />
+//     <button type="submit" class="btn">Get User</button>
+//   </form>
+// </div> */}
+
+var searchbodyEl = document.querySelector("searchbody");
+var cityinput = document.querySelector("city");
+
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    console.log(event);
+}
+
+var city = searchbodyEl.ariaValueMax.trim();
+    if (city) {
+        getcoords(ciity);
+        //clear content needed
+    } 
+    else {
+        alert('No city found.')
+    }
+
+var getcoords = function() {
+    //make request to URL
+    fetch('http://api.openweathermap.org/geo/1.0/direct?q=' + city + '&limit=1&appid=6c6a2773e4ad1859b9bab1adec8ab957').then(function(response) {
+        response.json().then(function(data) {
+        console.log(data);
         });
-    });
+    })
 };
+getcoords();
+
+
+
+searchbodyEl.addEventListener ("submit", formSubmitHandler);
