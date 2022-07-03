831d010a77a8c8d2c6f5f9c7128806b76a8e186a
Sczygelski <avamariescz@gmail.com>
2022-07-03 17:45:28 -0500

    trying anything

--- assets/script.js
+++ assets/script.js
@@ -1,44 +1,61 @@
 
 
-//6c6a2773e4ad1859b9bab1adec8ab957
-
-// {/* <div class="card">
-//   <h3 class="card-header text-uppercase">Search By User</h3>
-//   <form id="user-form" class="card-body">
-//     <label class="form-label" for="username">Username</label>
-//     <input name="username" id="username" type="text" autofocus="true" class="form-input" />
-//     <button type="submit" class="btn">Get User</button>
-//   </form>
-// </div> */}
-
-var searchbodyEl = document.querySelector("searchbody");
-var cityinput = document.querySelector("city");
+// var searchbodyEl = document.querySelector("#searchbody");
+// var cityEl = document.querySelector("#city");
 
 
-var formSubmitHandler = function(event) {
-    event.preventDefault();
-    console.log(event);
-}
+// //submite button works with this
+// var formSubmitHandler = function(event) {
+//     event.preventDefault();
+//     console.log(event);
+// }
 
-var city = searchbodyEl.ariaValueMax.trim();
-    if (city) {
-        getcoords(ciity);
-        //clear content needed
-    } 
-    else {
-        alert('No city found.')
-    }
 
-var getcoords = function() {
-    //make request to URL
-    fetch('http://api.openweathermap.org/geo/1.0/direct?q=' + city + '&limit=1&appid=6c6a2773e4ad1859b9bab1adec8ab957').then(function(response) {
+//get arrey for coordinates
+// var getcoords = function(city) {
+//     //searchbodyEl.addEventListener ("submit", formSubmitHandler);
+//     // var city1 = () => {
+//     //     let inputValue=cityEl.innerText;
+//     //     //document.getElementById("citytitle").innerHTML=inputValue;
+//     //     console.log(inputValue)
+//     //}
+//     inputValue = "Chicago";
+//     //make request to URL
+//     fetch('http://api.openweathermap.org/geo/1.0/direct?q=' + inputValue + '&limit=1&appid=6c6a2773e4ad1859b9bab1adec8ab957').then(function(response) {
+//         response.json().then(function(data) {
+//            console.log(data);
+//         });
+//     })
+//     // var cityobject = data[0];
+//     // var lat = cityobject.lat;
+//     // var long = cityobject.long;
+// };
+// getcoords();
+
+// var response = fetch('http://api.openweathermap.org/geo/1.0/direct?q=London&limit=1&appid=6c6a2773e4ad1859b9bab1adec8ab957');
+// console.log(response);
+
+
+// var formSubmitHandler = function(event) {
+//     event.preventDefault();
+// }
+
+// function getcities (){
+//     let inputValue = document.getElementsByClassName("citytitle")[1].value;
+//     document.getElementById("city").innerHTML=inputValue;
+//     console.log(inputValue)
+// }
+
+
+var getcoords = function(city) {
+    var apiURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=6c6a2773e4ad1859b9bab1adec8ab957";
+
+    fetch(apiURL).then(function(response) {
+        console.log(response);
         response.json().then(function(data) {
-        console.log(data);
+            console.log(data);
         });
-    })
+    });
 };
-getcoords();
 
-
-
-searchbodyEl.addEventListener ("submit", formSubmitHandler);
+getcoords("London");
